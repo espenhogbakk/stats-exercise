a2d84899ca35dcb10f79d2b7407e571a855398bc
Espen Høgbakk <espen@hogbakk.no>
2016-09-16 00:12:33 +0200

    Add more dates to the dataset

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,6 @@ BULK_SIZE = 10000 # Number of records to insert into db in one transaction
 
 # Since report #2 need to have at least 10 urls, and 5 referrers I added a
 # couple of other urls than what was given in the instructions.
-
 URLS = [
   'http://apple.com',
   'https://apple.com',
@@ -27,7 +26,7 @@ REFERRERS = [
   nil
 ]
 
-DATES = (15.days.ago.to_date..Date.today).map{ |date| date.to_datetime }
+DATES = (60.days.ago.to_date..Date.today).map{ |date| date.to_datetime }
 
 DB = PageView.db # Sequel DB connection
 
